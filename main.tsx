da1a125c3d3053b1cbbba1cec93d8859234cde51
neutah <kodabathulasiri@gmail.com>
2026-03-18 03:08:34 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
