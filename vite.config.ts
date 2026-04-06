4db4aa069297d80d51572937cc147bead0556094
neutah <kodabathulasiri@gmail.com>
2026-04-06 11:28:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
+import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",
   server: {
     host: "::",
     port: 8080,
-    hmr: {
-      overlay: false,
+  },
+  plugins: [
+    react(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
 }));
