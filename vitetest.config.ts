6648d630010864fbfa74f34be0a467e919a329c7
neutah <kodabathulasiri@gmail.com>
2026-04-06 11:12:57 -0400

    Update vitetest.config.ts

--- vitetest.config.ts
+++ vitetest.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SoundMap/',
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["./setupTests.ts"], 
+    setupFiles: ["./setup.ts"],
     include: ["**/*.{test,spec}.{ts,tsx}"],
-  },
-  resolve: {
-    alias: { 
-      "@": path.resolve(__dirname, "./") 
-    },
   },
 });
